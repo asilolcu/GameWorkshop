7043cb4cf13f36481a7c94646e197d3d14d6a03e
Ateş Balcı <atesbalci@gmail.com>
2017-11-04 16:25:09 +0300

    added player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,15 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour {
+public class Player : MonoBehaviour
+{
+    public Transform Planet;
 
-	// Use this for initialization
-	void Start () {
-		
+    private Rigidbody2D _body;
+
+    void Start()
+    {
+        _body = GetComponent<Rigidbody2D>();
+    }
+
+	void Update ()
+    {
+		Planet.Rotate(Vector3.back, -Time.deltaTime * 10);
+        if (Input.GetKey(KeyCode.Space))
+        {
+            _body.velocity = new Vector2(0, 6);
+        }
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        Destroy(gameObject);
+    }
 }
